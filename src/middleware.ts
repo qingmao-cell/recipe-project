622d5a34e4ffe1b5016b38b38007b6a2d8b807d2
qingmao-cell <youchiliao@gmail.com>
2025-08-21 12:58:44 +0900

    Fix 404 error by adjusting middleware routing

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,17 +8,13 @@ export default createMiddleware({
   defaultLocale: 'zh',
 
   // The prefix for the locale in the URL
-  localePrefix: 'always'
+  localePrefix: 'as-needed'
 });
 
 export const config = {
   // Match only internationalized pathnames
   matcher: [
-    // Match all pathnames except for
-    // - … if they start with `/api`, `/_next` or `/_vercel`
-    // - … the ones containing a dot (e.g. `favicon.ico`)
-    '/((?!api|_next|_vercel|.*\\..*).*)',
-    // However, match all pathnames within `/api`, except for health
-    '/api/((?!health).*)'
+    '/',
+    '/(zh|ja)/:path*'
   ]
 };
