f2cce84cb2a9c50d68ec50802a7f0cfffe3360ca
qingmao-cell <youchiliao@gmail.com>
2025-08-21 13:03:06 +0900

    Fix 404 by adding redirect to Chinese locale

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function RootPage() {
-  // This page should not be reached as middleware handles redirects
-  // But if it is, we'll return null
-  return null;
+  redirect('/zh');
 }
